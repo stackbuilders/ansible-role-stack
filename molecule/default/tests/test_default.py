aeadc9b199c71603b706e3045b5be846fb3496b1
Sebastián Estrella <sestrella.me@gmail.com>
2019-10-08 16:39:28 -0500

    Setup Tox / Test stack 2.1.3 and 1.9.3
    Co-authored-by: Carlos Egüez <tao.ceel@gmail.com>
    Co-authored-by: Nicolas Vivar <nickovivar@gmail.com>

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,8 +6,13 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_binary(host):
-    cmd = host.run('/usr/local/bin/stack --version')
-
+def test_stack_2_1_3_version(host):
+    cmd = host.run('/root/stack-2.1.3-linux-x86_64-static/stack --version')
     assert cmd.rc == 0
-    assert '1.9.1' in cmd.stdout
+    assert '2.1.3' in cmd.stdout
+
+
+def test_stack_1_9_3_version(host):
+    cmd = host.run('/root/stack-1.9.3-linux-x86_64-static/stack --version')
+    assert cmd.rc == 0
+    assert '1.9.3' in cmd.stdout
