5646f86eb7ad57d387133207f356534f40f4a643
Sebastián Estrella <sestrella.me@gmail.com>
2019-10-10 16:03:02 -0500

    Standardize extraction directory

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,12 +7,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_stack_2_1_3_version(host):
-    cmd = host.run('/root/stack-2.1.3-linux-x86_64-static/stack --version')
+    cmd = host.run('/root/stack-2.1.3/stack --version')
     assert cmd.rc == 0
     assert '2.1.3' in cmd.stdout
 
 
 def test_stack_1_9_3_version(host):
-    cmd = host.run('/root/stack-1.9.3-linux-x86_64-static/stack --version')
+    cmd = host.run('/root/stack-1.9.3/stack --version')
     assert cmd.rc == 0
     assert '1.9.3' in cmd.stdout
